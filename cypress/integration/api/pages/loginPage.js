b99ba3bff983981aeddf1006999ae3e530b51805
sumank <suman.kumar@devstringx.com>
2024-09-29 19:42:28 +0530

    myupdatedcode

--- cypress/integration/api/pages/loginPage.js
+++ cypress/integration/api/pages/loginPage.js
@@ -2,7 +2,6 @@ import { authenticator } from 'otplib';
 /// <reference types = "cypress"/>
 
 export const doLogin = () => {
-    // ******************* LOGIN AND GET AUTH KEY ******************** //
     const tokenvalue = authenticator.generate("EQ2WUPX7QC3VIPGPMQ5M5AOYADH4XMHT");
     cy.readFile('cypress/fixtures/api_login.json').then((data) => {
         data.token = parseInt(tokenvalue);
@@ -21,34 +20,25 @@ export const doLogin = () => {
 
 }
 export const doDashboardLogin = () => {
-    const tokenvalue = authenticator.generate("WP465RPUQHEL744Z3EQJ5XNJ7F5NQKQT");
-    cy.readFile('cypress/fixtures/api_DashboardLogin.json').then((data) => {
-        data.token = parseInt(tokenvalue);
-        cy.writeFile('cypress/fixtures/api_DashboardLogin.json', JSON.stringify(data));
-    });
     return cy.fixture('api_DashboardLogin.json').then((myFixture) => {
         cy.request({
             method: 'POST',
-            url: Cypress.env('baseUrl') + Cypress.env('loginEndPoint'),
+            url: Cypress.env('baseUrl') + Cypress.env('normallogin'),
             retryOnStatusCodeFailure: true,
             body: myFixture
         }).then((response) => {
             return response;
         })
     })
+	
 
 }
 
 export const doNewPrdLogin = () => {
-    const tokenvalue = authenticator.generate("EQ2WUPX7QC3VIPGPMQ5M5AOYADH4XMHT");
-    cy.readFile('cypress/fixtures/api_login.json').then((data) => {
-        data.token = parseInt(tokenvalue);
-        cy.writeFile('cypress/fixtures/api_login.json', JSON.stringify(data));
-    });
-    return cy.fixture('api_login.json').then((myFixture) => {
+    return cy.fixture('api_DashboardLogin.json').then((myFixture) => {
         cy.request({
             method: 'POST',
-            url: Cypress.env('baseUrl') + Cypress.env('loginEndPoint'),
+            url: Cypress.env('baseUrl') + Cypress.env('normallogin'),
             retryOnStatusCodeFailure: true,
             body: myFixture
         }).then((response) => {
